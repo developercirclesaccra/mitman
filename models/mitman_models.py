38789aec5c356c76dc7dd823b7801f624e423691
EvaMwangi <mwangi.evalyne2016@gmail.com>
2017-11-11 14:01:41 +0000

    can get data in database users

--- models/mitman_models.py
+++ models/mitman_models.py
@@ -6,5 +6,5 @@ class Users(BaseModel):
 	first_name = CharField(40)
 	last_name = CharField(40)
 	email = CharField(40)
-	phonenumber = CharField(40)
+	phone_number = CharField(40)
 	event = CharField(40)
--- server.py
+++ server.py
@@ -10,8 +10,8 @@ app = Flask(__name__)
 @app.before_first_request
 def initialize_tables():
 	connect_db()
-	if not FactModel.table_exists():
-		FactModel.create_table()
+	if not Users.table_exists():
+		Users.create_table()
 	disconnect_db()
 
 @app.before_request
@@ -24,3 +24,12 @@ def disconnect_db(err=None):
 
 
 app.secret_key = os.environ.get("FLASK_SECRET_KEY")
+
+@app.route('/users', methods=['GET'])
+def get_users():
+	query = Users.select()
+	
+	if query.count()<1:
+		return "Nothing to return"
+	
+	return jsonify({'users':query.get()._data})

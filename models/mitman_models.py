84e2fd564cf0473e14f227bac46f41b55c169ebe
EvaMwangi <mwangi.evalyne2016@gmail.com>
2017-11-14 22:15:08 +0000

    working on events

--- models/mitman_models.py
+++ models/mitman_models.py
@@ -8,3 +8,18 @@ class Users(BaseModel):
 	email = CharField(40)
 	phone_number = CharField(40)
 	event = CharField(40)
+
+class Events(BaseModel):
+	name = CharField(40)
+	description = CharField(100)
+	venue = CharField(40)
+	date_time = DateTimeField
+	poster_image_link = CharField(100)
+	is_swag = BooleanField
+	hashtag = CharField(40)
+	organizer = CharField(40)
+	feedback_time = DateTimeField
+	start_time = DateTimeField
+	end_time = DateTimeField
+	topic = CharField(40)
+	speaker = CharField(40)
--- server.py
+++ server.py
@@ -12,6 +12,8 @@ def initialize_tables():
 	connect_db()
 	if not Users.table_exists():
 		Users.create_table()
+	if not Events.table_exists():
+		Events.create_table()
 	disconnect_db()
 
 @app.before_request
@@ -25,6 +27,7 @@ def disconnect_db(err=None):
 
 app.secret_key = os.environ.get("FLASK_SECRET_KEY")
 
+# getting all users
 @app.route('/users', methods=['GET'])
 def get_users():
 	query = Users.select()
@@ -34,6 +37,7 @@ def get_users():
 	
 	return jsonify({'users':query.get()._data})
 
+# getting one user with a specific id
 @app.route('/users/<int:number>', methods=['GET'])
 def get_one_user(number):
 	query = Users.select().where(Users.id == number)
@@ -42,7 +46,8 @@ def get_one_user(number):
 		return "Nothing to return"
 	
 	return jsonify({'users':query.get()._data})
-		
+	
+# adding a user	
 @app.route('/users', methods=['POST'])
 def add_users():
 	first_name = request.form.get('fname')
@@ -55,8 +60,34 @@ def add_users():
 	query.execute()
 	return "Query recorded"
 
-# cURL
-# Postman
+# add an event
+@app.route('/Events', methods=['POST'])
+def add_events():
+	name = request.form.get('name')
+	description = request.form.get('description')
+	venue = request.form.get('venue')
+	date_time = request.form.get('date_time')
+	poster_image_link = request.form.get('poster_image_link')
+	is_swag = request.form.get('is_swag')
+	hashtag = request.form.get('hashtag')
+	organizer = request.form.get('organizer')
+	feedback_time = request.form.get('feedback_time')
+	start_time = request.form.get('start_time')
+	end_time = request.form.get('end_time')
+	topic = request.form.get('topic')
+	speaker = request.form.get('speakers')
 
+	query = Users.insert(name=name,description=description,venue=venue,date_time=date_time,poster_image_link=poster_image_link,is_swag=is_swag,hashtag=hashtag,organizer=organizer,feedback_time=feedback_time,start_time=start_time,end_time=end_time,topic=topic,speaker=speaker)
+	query.execute()
+	return "Query recorded"
 
-# curl -X POST http://localhost:5000/users -d 'fname=Francis&lname=Addai&email=franci.@mest.com&phone=9320239023Y&event=Hello'
+# get all Events
+@app.route('/Events', methods=['GET'])
+def get_events():
+	query = Events.select()
+	
+	if query.count()<1:
+		return "Nothing to return"
+	
+	return jsonify({'events':query.get()._data})
+

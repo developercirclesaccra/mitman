f9613dca85782d19f512fbc83c861f0b1b9b2f95
John Otu <johnne01@gmail.com>
2017-11-22 06:15:05 +0000

    Added created eventId to ref in parametric code Changes to be committed: 	modified:   routes/submit-event.js

--- routes/submit-event.js
+++ routes/submit-event.js
@@ -15,46 +15,77 @@ router.post('/', (req, res) => {
   let senderId = eventJSON.fbId;
 
   let getUserArgs = {
-    url_String: process.env.BACKEND_URL + '/user/' + senderId,
+    url_String: process.env.BACKEND_URL + 'user/' + senderId,
     method_String: 'GET',
-
-  }
-  let codeArgs = {
-    url_String: "https://graph.facebook.com/v2.6/me/messenger_codes",
-    method_String: 'POST',
     headers_Object: {
-      "content-type": "application/json"
-    },
-    qs_Object: {
-      access_token: process.env.PAGE_ACCESS_TOKEN
-    },
-    jsonData_Object: {
-      type: "standard",
-      data: {
-        ref: senderId + '_event_'
-      },
-      image_size: 1000
+      "mitman-token": process.env.MITMAN_TOKEN,
     },
   };
-  requestCall(codeArgs, (error, response, body) => {
-    if (error) {
-      throw error;
-    };
-    let eventCode = body.uri;
-    let imageJSON = {
-      "attachment": {
-        "type": "image",
-        "payload": {
-          "url": eventCode,
+  requestCall(getUserArgs, (err, resp, body) => {
+    if (err) { throw err };
+    let userData = JSON.parse(body);
+    if (userData.success) {
+      console.log('about to add organizer tojson...');
+      eventJSON.organizer = userData.user._id;
+      let addEventArgs = {
+        url_String: process.env.BACKEND_URL + 'event',
+        method_String: 'POST',
+        headers_Object: {
+          "content-type": "application/json",
+          "mitman-token": process.env.MITMAN_TOKEN,
+        },
+        jsonData_Object: {
+          event: eventJSON,
+        },
+      };
+      requestCall(addEventArgs, (err, resp, body) => {
+        if (err) { throw err };
+        if (body.success) {
+          let eventId = body.event._id;
+          console.log('*created event Id: ', eventId);
+          let codeArgs = {
+            url_String: "https://graph.facebook.com/v2.6/me/messenger_codes",
+            method_String: 'POST',
+            headers_Object: {
+              "content-type": "application/json"
+            },
+            qs_Object: {
+              access_token: process.env.PAGE_ACCESS_TOKEN
+            },
+            jsonData_Object: {
+              type: "standard",
+              data: {
+                ref: 'organizer_' + senderId + '_event_' + eventId,
+              },
+              image_size: 1000
+            },
+          };
+          requestCall(codeArgs, (error, response, body) => {
+            if (error) {
+              throw error;
+            };
+            let eventCode = body.uri;
+            let imageJSON = {
+              "attachment": {
+                "type": "image",
+                "payload": {
+                  "url": eventCode,
+                }
+              }
+            };
+            sendApi.sendMessage(senderId, { text: 'Please print the image below to be scanned by attendees at your meetup.' }, () => {
+              sendApi.sendMessage(senderId, imageJSON);
+            });
+            return;
+          })
+          return;
         }
-      }
-    };
-    sendApi.sendMessage(senderId, { text: 'Please print the image below to be scanned by attendees at your meetup.' }, () => {
-      sendApi.sendMessage(senderId, imageJSON);
-    });
-    return;
+        return console.log('*add event result: ', body)
+      })
+    }
+    return console.log('*get user from fbid: ', body);
   })
-  return;
+  
 });
 
 module.exports = router;

5337c0e21f0eafc24f22fd859214ada8fb9481fd
John Otu <johnne01@gmail.com>
2017-12-01 18:33:56 +0000

    Displayed options on code scan.
    Took care of before and after meetup date and start_time.
    Added momentjs module for date manipulation and formating.
    Changes to be committed:
    	modified:   mitman/receive-api.js
    	modified:   package.json

--- mitman/receive-api.js
+++ mitman/receive-api.js
@@ -4,6 +4,7 @@ const sendApi = require('./send-api');
 const getUserProfile = require('../utils/get-user-profile');
 const requestCall = require('../utils/request-call');
 const dialogs = require('../utils/dialogs');
+const moment = require('moment');
 
 const handlePostback = event => {
   const payload = event.postback.payload;
@@ -97,7 +98,7 @@ const handleReferral = event => {
   const eventId = ref.split('_')[3];
 
   let getEventArgs = {
-    url_String: process.env.BACKEND_URL + "event/" + eventId,
+    url_String: process.env.BACKEND_URL + "meetup/" + eventId,
     method_String: 'GET',
     headers_Object: {
       "mitman-token": process.env.MITMAN_TOKEN,
@@ -107,7 +108,53 @@ const handleReferral = event => {
     if (err) {
       throw err;
     };
-    return console.log('*Get event result: ', body);
+    console.log('*Get event result: ', body, typeof body);
+    let meetupData = JSON.parse(body);
+    if (meetupData.success) {
+      let meetup = meetupData.meetup;
+      let meetTime = `${meetup.date} ${meetup.start_time}`;
+      if (moment(meetTime).isBefore()) {
+        let msg = `Your ${meetup.name} meetup was held ${moment(meetTime).fromNow()}. `;
+        sendApi.sendMessage(senderId, { text: msg }, () => {
+          sendApi.sendMessage(senderId, {
+            "text": "Would you like to give feedback?",
+            "quick_replies": [
+              {
+                "content_type": "text",
+                "title": "Give feedback",
+                "payload": `givefeedback_${meetup._id}`
+              }
+            ]
+          });
+        });
+        return;
+      } else if (moment(meetTime).isAfter()) {
+        let msg = `Your ${meetup.name} meetup is holding ${moment(meetTime).fromNow()}. `;
+        sendApi.sendMessage(senderId, { text: msg }, () => {
+          sendApi.sendMessage(senderId, {
+            "text": "What would you like to do?",
+            "quick_replies": [
+              {
+                "content_type": "text",
+                "title": "View agenda",
+                "payload": `viewagenda_${meetup._id}`
+              },
+              {
+                "content_type": "text",
+                "title": "Reserve SWAG",
+                "payload": `reserveswag_${meetup._id}`
+              },
+              {
+                "content_type": "text",
+                "title": "Give feedback",
+                "payload": `givefeedback_${meetup._id}`
+              }
+            ]
+          });
+        });
+        return;
+      }
+    }
   })
 }
 
